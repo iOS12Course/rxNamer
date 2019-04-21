dd00ffb1fc3a0321d1ee513d9f5708613b6a9fea
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-04-21 13:30:05 -0500

    Setting up UI

--- rxNamer/ViewController.swift
+++ rxNamer/ViewController.swift
@@ -12,6 +12,12 @@ import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLbl: UILabel!
+    @IBOutlet weak var nameEntryTextField: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    @IBOutlet weak var namesLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
